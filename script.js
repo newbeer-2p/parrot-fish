8ec333b896ea2b3c74804bf22a3cf38bb72bf4a5
Newbeer <newbeerpongsakorn@gmail.com>
2021-11-06 12:57:46 +0700

    Update Main3

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let window_height = window.innerHeight;
+let main_part =  document.querySelector("#main-part");
 
 let body = document.body;
 
 window.onscroll = function(){
     let scrollY = window.scrollY;
 
-    if (scrollY == 0){
-        body.dataset.mode = "home";
-    }
+    main_part.style.setProperty("--sy", scrollY)
+    // if (scrollY+window_height >= document.querySelector("#main4").offsetTop+1100){
+    //     window.scroll(0, document.querySelector("#main4").offsetTop)
+    // }
+
+
+
+    // if (scrollY == 0){
+    //     body.dataset.mode = "home";
+    // }
+    // else{
+    //     body.dataset.mode = "main";
+    // }
 }
 
 function goto(page){
-    if (page === "first"){
-        body.dataset.mode = "main";
-        window.scroll(0, window_height);
-    }
+    let des_page = document.querySelector(`#${page}`);
+    // if (page === "main2"){
+    //     body.dataset.mode = "main";
+    window.scroll(0, des_page.offsetTop);
+    // }
 }
