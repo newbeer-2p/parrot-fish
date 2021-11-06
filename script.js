e0fbd1753ef34ef75af36828920856ef3ba62459
qxm <56361045+gxmmmm@users.noreply.github.com>
2021-11-06 17:37:17 +0700

    add animate

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let window_height = window.innerHeight;
-let main_part =  document.querySelector("#main-part");
+let main_part = document.querySelector("#main-part");
 
 let body = document.body;
 
-window.onscroll = function(){
+window.onscroll = function() {
     let scrollY = window.scrollY;
+    console.log(window.scrollY)
     let main3 = document.querySelector("#main3");
-    if (scrollY <= (main3.offsetTop + document.querySelector("#text2-in-main3").offsetTop - 250) && scrollY >= main3.offsetTop){
+    if (scrollY <= (main3.offsetTop + document.querySelector("#text2-in-main3").offsetTop - 250) && scrollY >= main3.offsetTop) {
         document.querySelector("#parrot-in-main3").dataset.show = "1";
         document.querySelector("#parrot-in-main3").style.setProperty("--mvY", scrollY - main3.offsetTop);
     }
@@ -21,10 +22,10 @@ window.onscroll = function(){
     // }
 }
 
-function goto(page){
+function goto(page) {
     let des_page = document.querySelector(`#${page}`);
     window.scroll(0, des_page.offsetTop);
-    if (page === "main3"){
+    if (page === "main3") {
         document.querySelector("#parrot-in-main3").dataset.show = "1";
     }
 }
