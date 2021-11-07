cb3705b9f64b49ce1b44f83236e4d85d979d8676
Newbeer <newbeerpongsakorn@gmail.com>
2021-11-07 18:39:08 +0700

    edit Parrot Animation

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ let body = document.body;
 
 window.onscroll = function() {
     let scrollY = window.scrollY;
-    console.log(window.scrollY)
+    // console.log(window.scrollY)
     let main3 = document.querySelector("#main3");
     if (scrollY <= (main3.offsetTop + document.querySelector("#text2-in-main3").offsetTop - 250) && scrollY >= main3.offsetTop) {
-        document.querySelector("#parrot-in-main3").dataset.show = "1";
-        document.querySelector("#parrot-in-main3").style.setProperty("--mvY", scrollY - main3.offsetTop);
+        let parrot_fish = document.querySelector("#parrot-in-main3");
+        parrot_fish.dataset.show = "1";
+        parrot_fish.style.setProperty("--mvY", scrollY - main3.offsetTop);
+        parrot_fish.style.setProperty("--scH", main3.offsetTop + document.querySelector("#text2-in-main3").offsetTop - 250 - main3.offsetTop);
+        
     }
 
 
